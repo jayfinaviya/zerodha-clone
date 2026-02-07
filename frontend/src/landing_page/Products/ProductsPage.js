1835d21a2c3f3f3516a8257c52b61d53326af403
jay finaviya <jayfinaviya5@gmail.com>
2026-02-07 15:16:56 +0530

    add LeftSection Products

--- frontend/src/landing_page/Products/ProductsPage.js
+++ frontend/src/landing_page/Products/ProductsPage.js
@@ -12,8 +12,35 @@ function ProductsPage() {
     return ( 
         <>
         <Hero/>
-        <LeftSection/>
+         <LeftSection
+        imageURL="media/images/kite.png"
+        productName="Kite"
+        productDesription="Our ultra-fast flagship trading platform with streaming market data, advanced charts, an elegant UI, and more. Enjoy the Kite experience seamlessly on your Android and iOS devices."
+        tryDemo=""
+        learnMore=""
+        googlePlay=""
+        appStore=""
+        />
         <RightSection/>
+         <LeftSection
+        imageURL="media/images/coin.png"
+        productName="Coin"
+        productDesription="Buy direct mutual funds online, commission-free, delivered directly to your Demat account. Enjoy the investment experience on your Android and iOS devices."
+        tryDemo=""
+        learnMore=""
+        googlePlay=""
+        appStore=""
+      />
+      <RightSection/>
+      <LeftSection
+        imageURL="media/images/varsity.png"
+        productName="Varsity mobile"
+        productDesription="An easy to grasp, collection of stock market lessons with in-depth coverage and illustrations. Content is broken down into bite-size cards to help you learn on the go."
+        tryDemo=""
+        learnMore=""
+        googlePlay=""
+        appStore=""
+      />
         <Universe/>
         </>
 
