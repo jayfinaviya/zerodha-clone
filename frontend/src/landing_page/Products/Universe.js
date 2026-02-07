e02c5afedee1c72b57bbfdb66fde6b196617e432
jay finaviya <jayfinaviya5@gmail.com>
2026-02-07 16:45:46 +0530

    add Universe Products

--- frontend/src/landing_page/Products/Universe.js
+++ frontend/src/landing_page/Products/Universe.js
@@ -2,7 +2,46 @@ import React from 'react'
 
 function Universe() {
     return ( 
-        <h1>Universe</h1>
+        <div className="container mt-5">
+      <div className="row text-center">
+        <h1>The Zerodha Universe</h1>
+        <p className="mt-3">
+          Extend your trading and investment experience even further with our
+          partner platforms
+        </p>
+
+        <div className="col-4 p-3 mt-5">
+          <img src="media/images/smallcaseLogo.png" />
+          <p className="text-small text-muted">Thematic investment platform</p>
+        </div>
+        <div className="col-4 p-3 mt-5">
+          <img src="media/images/streakLogo.png" />
+          <p className="text-small text-muted">Algo & strategy platform</p>
+        </div>
+        <div className="col-4 p-4 mt-5">
+          <img src="media/images/sensibullLogo.svg"/>
+          <p className="text-small text-muted mt-3">Options trading platform</p>
+        </div>
+        <div className="col-4 p-3 mt-5">
+          <img src="media/images/zerodhaFundhouse.png" />
+          <p className="text-small text-muted">Asset management</p>
+        </div>
+        <div className="col-4 p-3 mt-5">
+          <img src="media/images/goldenpiLogo.png" />
+          <p className="text-small text-muted">Bonds trading platform</p>
+        </div>
+        <div className="col-4 p-3 mt-5">
+          <img src="media/images/dittoLogo.png" />
+          <p className="text-small text-muted">Insurance</p>
+        </div>
+        <button
+          className="p-2 btn btn-primary fs-5 mb-5 mt-5"
+          style={{ width: "20%", margin: "0 auto" }}
+        >
+          Signup Now
+        </button>
+      </div>
+    </div>
      );
 }
 
