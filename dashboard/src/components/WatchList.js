73dd6a5c96131fb2147e2b02fe1e3b4cc9141088
jay finaviya <jayfinaviya5@gmail.com>
2026-02-15 14:33:09 +0530

    Plotting Graphs with ChartJS

--- dashboard/src/components/WatchList.js
+++ dashboard/src/components/WatchList.js
@@ -1,6 +1,11 @@
-import React from "react";
+import React,{useState} from "react";
 
 import { Tooltip, Grow } from "@mui/material";
+
+import {
+  KeyboardArrowDown,
+  KeyboardArrowUp,
+} from "@mui/icons-material";
 
 import { watchlist } from "../data/data";
 
@@ -20,7 +25,7 @@ const WatchList = () => {
 
             <ul className="list">
                  {watchlist.map((stock, index) => {
-                    return <p>{stock.name}</p>;
+                    return <WatchListItem stock={stock} key={index} />;
                 })}
             </ul>
         </div>
@@ -28,3 +33,32 @@ const WatchList = () => {
 };
 
 export default WatchList;
+
+const WatchListItem = ({ stock }) => {
+  const [showWatchlistActions, setShowWatchlistActions] = useState(false);
+
+  const handleMouseEnter = (e) => {
+    setShowWatchlistActions(true);
+  };
+
+  const handleMouseLeave = (e) => {
+    setShowWatchlistActions(false);
+  };
+
+   return (
+    <li onMouseEnter={handleMouseEnter} onMouseLeave={handleMouseLeave}>
+      <div className="item">
+        <p className={stock.isDown ? "down" : "up"}>{stock.name}</p>
+        <div className="itemInfo">
+          <span className="percent">{stock.percent}</span>
+          {stock.isDown ? (
+            <KeyboardArrowDown className="down" />
+          ) : (
+            <KeyboardArrowUp className="down" />
+          )}
+          <span className="price">{stock.price}</span>
+        </div>
+      </div>
+    </li>
+  );
+};
