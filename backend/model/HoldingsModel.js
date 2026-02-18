38955bcfe40ec5fbfd8b8ffb22eb10da025be0bb
jay finaviya <jayfinaviya5@gmail.com>
2026-02-18 23:12:39 +0530

    Connecting MongoDB with Express

--- backend/model/HoldingsModel.js
+++ backend/model/HoldingsModel.js
@@ -1,7 +1,7 @@
-const {module} = require("mongoose");
+const { model} = require("mongoose");
 
 const {HoldingsSchema} = require("../schemas/HoldingsSchema");
 
-const HoldingsModel = new module("holding",HoldingsSchema);
+const HoldingsModel = new  model ("holding",HoldingsSchema);
 
 module.exports = {HoldingsModel};
