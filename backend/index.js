97248238a6192c3afdcf0432ab96d19ecf610b6f
jay finaviya <jayfinaviya5@gmail.com>
2026-02-19 23:00:42 +0530

    Connecting MongoDB with Express

--- backend/index.js
+++ backend/index.js
@@ -10,6 +10,8 @@ const { HoldingsModel } = require("./model/HoldingsModel");
 
 const { PositionsModel } = require("./model/PositionsModel");
 
+const { OrdersModel } = require("./model/OrdersModel");
+
 const PORT = process.env.PORT || 3002;
 const uri = process.env.MONGO_URL;
 
@@ -196,6 +198,19 @@ app.get("/allHoldings", async (req, res) => {
 app.get("/allPositions", async (req, res) => {
   let allPositions = await PositionsModel.find({});
   res.json(allPositions);
+});
+
+app.post("/newOrder", async (req, res) => {
+  let newOrder = new OrdersModel({
+    name: req.body.name,
+    qty: req.body.qty,
+    price: req.body.price,
+    mode: req.body.mode,
+  });
+
+  newOrder.save();
+
+  res.send("Order saved!");
 });
 
 app.listen(PORT,()=>{
